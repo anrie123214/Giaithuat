cd1606e6abcd5916862dea0a9fed7da442c506f8
Blu laptop <promi@DESKTOP-RIQE689.localdomain>
2026-08-10 13:22:07 +0700

    finish tuan 12

--- tailieutuan12/baitaptuan12/bai1.cpp
+++ tailieutuan12/baitaptuan12/bai1.cpp
@@ -61,10 +61,133 @@ void CreateBSTree(BTree &t,ifstream &fin)
         InsertNode(t.root,x);
     }
 }
+void Descend(BNode* p, queue<int>& Q);
+void Descend(BNode* p, queue<int>& Q)
+{
+    if (p == NULL)
+    {
+        return;
+    }
+    Descend(p->Right,Q);
+    Q.push(p->data);
+    Descend(p->Left,Q);
+}
+
+bool Search(BNode* p, int x);
+bool Search(BNode* p, int x)
+{
+    if (p == NULL)//to leaf node not found return false
+    {
+        return false;
+    }
+    if (p->data == x)
+        return true;//found return true
+    if (x < p->data)
+        return Search(p->Left,x);//lower go left
+    return Search(p->Right,x);//upper go right
+}
+int SumLeaves(BNode* p);
+int SumLeaves(BNode* p)
+{
+    if (p == NULL)
+    {
+        return 0;
+    }
+    if (p->Left == NULL && p->Right == NULL)
+    {
+        return p->data;
+    }
+    return SumLeaves(p->Left) + SumLeaves(p->Right);
+}
+int findMin(BNode* p);
+int findMin(BNode* p)
+{
+    if (p == NULL)
+        return 0;
+    while (p->Left != NULL)
+    {
+        p = p->Left;
+    }
+    if (p->Left == NULL)
+    {
+        return p->data;
+    }
+}
+int findMax(BNode* p);
+int findMax(BNode* p)
+{
+    if (p == NULL)
+        return 0;
+    while (p->Right != NULL)
+        p = p->Right;
+    if (p->Right == NULL)
+        return p->data;
+}
+int GetHeight(BNode* p);
+int GetHeight(BNode* p)
+{
+    if (p == NULL)
+        return 0;
+    int a = GetHeight(p->Left);
+    int b = GetHeight(p->Right);
+    int max = a > b ? a : b;
+    return (max + 1);
+}
+BNode* GetMostLeft(BNode* p);
+BNode* GetMostLeft(BNode* p)
+{
+    if (p == NULL)
+        return 0;
+    if (p->Left == NULL && p->Right == NULL)
+        return p;
+    BNode* result = GetMostLeft(p->Left);
+    if (result != NULL)
+        return result;
+    return GetMostLeft(p->Right);
+}
+BNode* GetMostRight(BNode* p)
+{
+    if (p == NULL)
+        return 0;
+    if (p->Left == NULL && p->Right == NULL)
+        return p;
+    BNode* result = GetMostRight(p->Right);
+    if (result != NULL)
+        return result;
+    return GetMostRight(p->Left);
+}
+void swapLeaves(BTree& t);
+void swapLeaves(BTree& t)
+{
+    if (t.root == NULL)
+        return;
+    BNode* left = GetMostLeft(t.root);
+    BNode* right = GetMostRight(t.root);
+    if (left == NULL || right == NULL)
+        return;
+    int temp = left->data;
+    left->data = right->data;
+    right->data = temp;
+}
 int main()
 {
     BTree t;
     ifstream fin("input.txt");
     Initialize(t);
     CreateBSTree(t,fin);
+    queue<int> Q;
+    Descend(t.root, Q);
+    int needSearch;
+    cout<<"Nhap can tim"<<endl;
+    cin>>needSearch;
+    bool found = Search(t.root,needSearch);
+    int sumResult = SumLeaves(t.root);
+    cout<<"Tong la "<<sumResult<<endl;
+    int min = findMin(t.root);
+    cout << "Min = " << min << endl;
+    int max = findMax(t.root);
+    cout << "Max = " << max << endl;
+    int Height = GetHeight(t.root);
+    cout << "Height = " << Height << endl;
+    swapLeaves(t);
 }
